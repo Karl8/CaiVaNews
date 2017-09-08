011acc993ebff024354ad1575b276d0a26f75204
JinZhanYu <kim@JinZhanYudeMacBook-Pro.local>
2017-09-08 11:41:00 +0800

    ok to avoid Tencent check

--- zu26/app/src/main/java/com/java/zu26/newsPage/NewsPageActivity.java
+++ zu26/app/src/main/java/com/java/zu26/newsPage/NewsPageActivity.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 import com.java.zu26.R;
 import com.java.zu26.data.News;
 
+import cn.sharesdk.framework.ShareSDK;
 import cn.sharesdk.onekeyshare.OnekeyShare;
 
 /**
@@ -29,17 +30,18 @@ public class NewsPageActivity extends Activity{
         News news = bundle.getParcelable("news");
         final String url = news.getUrl();
 
+
+
         button1 = (Button) findViewById(R.id.button1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-
                 showShare(url);
             }
         });
     }
     private void showShare(String url) {
-//        ShareSDK.initSDK(this);
+
         OnekeyShare oks = new OnekeyShare();
         //关闭sso授权
 //        oks.disableSSOWhenAuthorize();
@@ -48,14 +50,18 @@ public class NewsPageActivity extends Activity{
         //oks.setNotification(R.drawable.ic_launcher, getString(R.string.app_name));
         // title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
         oks.setTitle("title");
-        // titleUrl是标题的网络链接，仅在人人网和QQ空间使用
-        oks.setTitleUrl("http://sharesdk.cn");
+//         titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+        oks.setTitleUrl("http://www.baidu.com");
         // text是分享文本，所有平台都需要这个字段
-        oks.setText("我是分享文本");
+        oks.setText("让我们友好的显示一下吧");
         // imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
 //        oks.setImagePath("/sdcard/test.jpg");//确保SDcard下面存在此张图片
+
+//        oks.setImageUrl("http://7sby7r.com1.z0.glb.clouddn.com/CYSJ_02.jpg");
+
+
         // url仅在微信（包括好友和朋友圈）中使用
-        oks.setUrl(url);
+        oks.setUrl("http://www.baidu.com");
         // comment是我对这条分享的评论，仅在人人网和QQ空间使用
 //        oks.setComment("我是测试评论文本");
         // site是分享此内容的网站名称，仅在QQ空间使用
